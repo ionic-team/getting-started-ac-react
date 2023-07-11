551d47714a0928d5eb3e29425be8e59d4122b454
Ken Sodemann <ken@ionic.io>
2023-07-11 14:45:49 -0500

    feat(auth): allow configuration via .env file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import { expect, test } from 'vitest';
 import App from './App';
 
 test('renders without crashing', () => {
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -6,12 +6,12 @@ import { SessionVaultContext } from './SessionVaultProvider';
 const isNative = isPlatform('hybrid');
 
 const options: ProviderOptions = {
-  audience: 'https://io.ionic.demo.ac',
-  clientId: 'yLasZNUGkZ19DGEjTmAITBfGXzqbvd00',
-  discoveryUrl: 'https://dev-2uspt-sz.us.auth0.com/.well-known/openid-configuration',
-  logoutUrl: isNative ? 'msauth://login' : 'http://localhost:8100/login',
-  redirectUri: isNative ? 'msauth://login' : 'http://localhost:8100/login',
-  scope: 'openid offline_access email picture profile',
+  audience: import.meta.env.VITE_AUDIENCE,
+  clientId: import.meta.env.VITE_CLIENTID,
+  discoveryUrl: import.meta.env.VITE_DISCOVERYURL,
+  logoutUrl: isNative ? import.meta.env.VITE_LOGOUTURLMOBILE : import.meta.env.VITE_LOGOUTURLWEB,
+  redirectUri: isNative ? import.meta.env.VITE_REDIRECTURIMOBILE : import.meta.env.VITE_REDIRECTURIWEB,
+  scope: import.meta.env.VITE_SCOPE,
 };
 
 const setupAuthConnect = async (): Promise<void> => {
